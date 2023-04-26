1d21196bc3d1e7741973923142cb717d6ea45221
Hasanrza <75855975+hasanrza888@users.noreply.github.com>
2023-04-26 17:10:58 +0400

    Update sendOtpForm.jsx

--- src/components/sendOtpForm/sendOtpForm.jsx
+++ src/components/sendOtpForm/sendOtpForm.jsx
@@ -1,29 +1,52 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './sendOtpForm.css'
 import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
-import { useState } from 'react';
-import axios from 'axios';
-function SendOtpForm({setOpenOtpWindow, setNewPassword,email,setEmail,otp,setOtp}) {
-    const verifyOtpUrl = 'http://localhost:5000/api/verifyCompanyOtp'
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import NotificationMessage from '../notification_message/notification_message';
+function SendOtpForm({setOpenOtpWindow, setNewPassword, login, userSignUp}) {
     const [reverseAnimation, setReverseAnimation] = useState(false); 
+    const [countDown, setCountDown] = useState(120);
+    const [errorMessage, setErrorMessage] = useState({
+        errorCheck: false,
+        errorContent : ''
+    }); 
+    const [successMsg, setSuccessMsg] = useState(false);
+    const navigate = useNavigate();
+    const [notification_message_content, setNotification_message_content] = useState('');
     const sendOtpCodeHandle = (e)=>{
-        e.preventDefault();
-        axios.post(verifyOtpUrl,{email:email,otp:otp},{withCredentials:true}).then(dt=>{
-            setNewPassword(dt.data.succes);
-            console.log(dt.data)
-        }).catch(err=>console.log(err))
+        e.preventDefault();                
+        if(login === true){
+            setNewPassword(true);
+        }else if(userSignUp === true){
+            setSuccessMsg(true);
+            setNotification_message_content('Qeydiyyatınız uğurlu oldu !');
+            setTimeout(()=>{
+                navigate('/login');    
+            }, 5000);            
+        }
     }
     const closeWindowBox = ()=>{  
-        setReverseAnimation(true);
+        setReverseAnimation(true);        
         setTimeout(()=>{
             setOpenOtpWindow(false);  
-        },300)                
+        },300);            
     } 
+    useEffect(()=>{
+        const timer = countDown > 0 ? setInterval(()=>{
+            const timeOtp = countDown - 1;
+            setCountDown(timeOtp);
+        }, 1000) : null  
+         
+        return ()=> clearInterval(timer);
+    }, [countDown]);
+
     return ( 
         <div className={`send_top_form_container ${ reverseAnimation ? "send_top_form_close_animation" : "send_top_form_open_animation"}`}>
             {/* window close button */}
-            <div className="forgot_password_form_window_close">
-                <FontAwesomeIcon icon={faAngleLeft} onClick={closeWindowBox}/>
+            <div className="forgot_password_form_window_close" onClick={closeWindowBox}>
+                <FontAwesomeIcon icon={faAngleLeft} ></FontAwesomeIcon>
+                Geri
             </div>
             {/* form info */}
             <div className="forgot_password_form_info">
@@ -33,11 +56,19 @@ function SendOtpForm({setOpenOtpWindow, setNewPassword,email,setEmail,otp,setOtp
                 {/* otp code input */}
                 <label htmlFor="otpcodeipnut">
                     OTP Kod
-                    <input value={otp} onChange={(e)=>{setOtp(e.target.value)}} type="text" name='otpcodeinput' className="send_top_form_input" required/>
+                    <input type="text" name='otpcodeinput' className="send_top_form_input" required/>
                 </label>
+                {
+                    errorMessage ? <div className="send_otp_form_error_message">{errorMessage.errorContent}</div> : null
+                } 
+                {/* countdown for OTP */}
+                <div className='count_Down_Otp'>
+                    {countDown > 0 ? `${countDown} saniyə sonra bitəcək !` : 'Vaxt bitdi !'} 
+                </div>
                 {/* form confirm button */}
                 <input type="submit" value="Təsdiqlə" className="send_top_form_submit"/>
             </form>
+            {successMsg ? <NotificationMessage setSuccessMsg = {setSuccessMsg} notification_message_content = {notification_message_content} /> : null}
         </div>
     );
 }
