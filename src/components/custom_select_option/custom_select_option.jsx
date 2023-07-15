69d7610938d099fafcfc28f6419b4212e220768c
hasanrza888 <hasanrza.888@gmail.com>
2023-07-15 22:29:18 +0400

    filter boxes are ready

--- src/components/custom_select_option/custom_select_option.jsx
+++ src/components/custom_select_option/custom_select_option.jsx
@@ -3,36 +3,76 @@ import './custom_select_option.css';
 import { faSortDown } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 
-function CustomSelectOption({select_option_name, select_option_array}) {
+function CustomSelectOption({select_option_name, select_option_array, select_update}) {
     const [optionsShow, setOptionsShow] = useState(false);
+    const [choosedOption, setChoosedOption] = useState('');
     const select_option_opener = ()=>{
         setOptionsShow(!optionsShow);
     }
-    // window.addEventListener('click', (e)=>{
-    //     if(e.target.className !== "custom_options_container"){
-    //         setOptionsShow(!optionsShow);
-    //     }
-    //     console.log(e.target.className);
-    // });
+    const mainOptionNameClk = (item, index)=> {
+        if(choosedOption !== `${item['optionName']}`){
+            setChoosedOption(`${item['optionName']}`);     
+        }else{
+            setChoosedOption('');
+        }
+        // close options window 
+        setOptionsShow(false); 
+        // removes all selected main options
+        select_option_array.map((itemF, indexF) => {
+            if(indexF !== index){
+                itemF["selected"] = false;
+            }            
+            if(itemF['subOptions']){
+                for(let i=0; i<itemF['subOptions'].length; i++){
+                    itemF['subOptions'][i].selected = false;
+                }    
+            }            
+        }); 
+        select_option_array[index].selected = !select_option_array[index].selected;
+        select_update([...select_option_array]);
+    }
+    const subOptionNameClick = (subItem, subIndex, item, index)=>{
+        if(choosedOption !== `${subItem["subOptionsName"]}`){
+            setChoosedOption(`${subItem["subOptionsName"]}`);
+        }else{
+            setChoosedOption('');
+        }
+        // close options window        
+        setOptionsShow(false);
+        // removes all selected main options
+        select_option_array.map(itemF => {
+            itemF["selected"] = false;
+            if(itemF['subOptions']){
+                for(let i=0; i<itemF['subOptions'].length; i++){
+                    if(i !== subIndex){
+                        itemF['subOptions'][i].selected = false;    
+                    }                    
+                }    
+            }            
+        }); 
+        select_option_array[index]['subOptions'][subIndex].selected = !select_option_array[index]['subOptions'][subIndex].selected;
+        select_update([...select_option_array]);
+    }
     return ( 
         <div className="custom_select_option_container">
             {/* select name */}
-            <div className="custom_select_name_container" onClick={select_option_opener}>
-                <div className="custom_select_name">{select_option_name}</div>
+            <div className={`custom_select_name_container ${choosedOption ? 'custom_select_name_active' : ''}`} onClick={select_option_opener}>
+                <div className="custom_select_name">{choosedOption ? choosedOption : select_option_name}</div>
                 <FontAwesomeIcon icon={faSortDown} />
             </div>
             {/* showing or hiding options */}
             <div className={`custom_options_container ${optionsShow ? "custom_options_show" : "custom_options_hide"}`}>
-                <ul className="custom_options_list">
-                    {
+                <ul className="custom_options_lists">
+                    { 
                         select_option_array.map((item, index)=>{
-                            return <li key={item['categoryName']}>
-                                        <div className="custom_options_list_item" onClick={()=> console.log(item['categoryName'])}>{item['categoryName']}</div>
+                            return <li key={index} className='custom_options_list'>
+                                        <div className={`custom_options_list_item ${item['selected'] ? "custom_options_list_selected" : ''}`} onClick={()=> mainOptionNameClk(item, index)}>{item['optionName']}</div>
                                         <ul className='custom_options_sub_list'>
                                             {
-                                                item['subCategories'] ?
-                                                (item['subCategories'].map((subItem, subIndex)=>
-                                                    <li key={subItem} className='custom_options_sub_list_item' onClick={()=> console.log(subItem)}>{subItem}</li>
+                                                // ____________ suboptions list _______________
+                                                item['subOptions'] ?
+                                                (item['subOptions'].map((subItem, subIndex)=>
+                                                    <li key={subItem["subOptionsName"]} className={`custom_options_sub_list_item ${subItem["selected"] ? "custom_options_list_selected" : ''}`} onClick={()=> subOptionNameClick(subItem, subIndex, item, index)}>{subItem["subOptionsName"]}</li>
                                                 )) : null
                                             }                                    
                                         </ul>
--- src/pages/vacancies/vacancies.jsx
+++ src/pages/vacancies/vacancies.jsx
@@ -13,21 +13,163 @@ function Vacancies() {
     const [successMsg, setSuccessMsg] = useState(false);
     const [savedJob, setSavedJob] = useState('');
     const notification_message_content = <div>"{savedJob}" adlı vakansiya şəxsi hesabınızda <strong><u>sevimlilər</u></strong> bölməsinə əlavə olundu.</div>;
-    const categories = [
+    // categories
+    const [categories, setCategories] = useState([
         {
-            categoryName: 'IT',
-            subCategories: ['Web', 'Security', 'QA', 'Network']
+            optionName: 'IT',
+            selected: false,
+            subOptions: [
+                {subOptionsName: "Web", selected: false},
+                {subOptionsName: "QA", selected: false},
+                {subOptionsName: "Security", selected: false},
+            ]
         },
         {
-            categoryName: 'Bank',
-            subCategories: ['Kredit mütəxəssisi', 'Analitik', 'Əməliyyatçı', 'Şöbə müdiri']
+            optionName: 'Bank',  
+            selected: false,          
+            subOptions: [
+                {subOptionsName: "Kredit mütəxəssisi", selected: false},
+                {subOptionsName: "Analitik", selected: false},
+                {subOptionsName: "Əməliyyatçı", selected: false},
+                {subOptionsName: "Şöbə müdiri", selected: false}
+            ]
         },
         {
-            categoryName: 'Marketinq və Satış',
-            subCategories: '',
+            optionName: 'Marketinq və Satış',
+            selected: false,
+            subOptions: '',
         }
-    ]
-    
+    ])
+    // cities
+    const [jobCity, setJobCity] = useState([
+        {
+            optionName: 'Bakı',
+            selected: false,
+        },
+        {
+            optionName: 'Sumqayıt',
+            selected: false,
+        },
+        {
+            optionName: 'Lənkəran',
+            selected: false,
+        },
+        {
+            optionName: 'Masallı',
+            selected: false,
+        }
+    ])
+    // job experience
+    const [jobExperience, setJobExperience] = useState([
+        {
+            optionName: 'Təcrübəsiz',
+            selected: false,
+        },
+        {
+            optionName: '1 ildən aşağı',
+            selected: false,
+        },
+        {
+            optionName: '1 ildən 3 ilə qədər',
+            selected: false,
+        },
+        {
+            optionName: '3 ildən 5 ilə qədər',
+            selected: false,
+        },
+        {
+            optionName: '5 ildən çox',
+            selected: false,
+        }
+    ])
+    // education level
+    const [educationLevel, setEducationLevel] = useState([
+        {
+            optionName: 'Təhsilsiz',
+            selected: false,
+        },
+        {
+            optionName: 'Orta',
+            selected: false,
+        },
+        {
+            optionName: 'Ali',
+            selected: false,
+        }
+    ])
+    // company
+    const [company, setCompany] = useState([
+        {
+            optionName: 'İŞland',
+            selected: false,
+        },
+        {
+            optionName: 'Paşa bank',
+            selected: false,
+        },
+        {
+            optionName: 'Kapital bank',
+            selected: false,
+        },
+        {
+            optionName: 'Yelo bank',
+            selected: false,
+        },
+        {
+            optionName: 'ABB',
+            selected: false,
+        },
+        {
+            optionName: 'Pasha sığorta',
+            selected: false,
+        }
+    ])
+    // job type
+    const [jobType, setJobType] = useState([
+        {
+            optionName: 'Tam',
+            selected: false,
+        },
+        {
+            optionName: 'Natamam',
+            selected: false,
+        },
+        {
+            optionName: 'Onlayn',
+            selected: false,
+        },
+        {
+            optionName: 'Dəyişkən',
+            selected: false,
+        }
+    ])
+    // vacancy order
+    const [vacancyOrder, setVacancyOrder] = useState([
+        {
+            optionName: 'Ən Son',
+            selected: false,
+        },
+        {
+            optionName: 'İlk',
+            selected: false,
+        },
+        {
+            optionName: 'Ən çox maaş',
+            selected: false,
+        },
+        {
+            optionName: 'Ən az maaş',
+            selected: false,
+        },
+        {
+            optionName: 'Ən çox baxış sayı',
+            selected: false,
+        },
+        {
+            optionName: 'Ən az baxış sayı',
+            selected: false,
+        }        
+    ])
     return ( 
         <div className="vacancies_page_container">
             {/* image slider and job search container */}
@@ -55,9 +197,49 @@ function Vacancies() {
                     <div className="vacancies_page_filters_heading">Filterlər</div>
                     {/* filter boxes container */}
                     <ul className="vacancies_page_filters_boxes_container">
+                        {/* category */}
                         <li className="vacancies_page_filters_box">
-                            <CustomSelectOption select_option_name={'Kateqoriya'} select_option_array={categories} />
-                        </li>                        
+                            <CustomSelectOption select_option_name={'Kateqoriya'} select_option_array={categories} select_update={setCategories}/>
+                        </li>    
+                        {/* city */}
+                        <li className="vacancies_page_filters_box">
+                            <CustomSelectOption select_option_name={'Şəhər'} select_option_array={jobCity} select_update={setJobCity}/>
+                        </li>  
+                        {/* salary                   */}
+                        <li className="vacancies_page_filters_box">
+                            <div className="vacancies_page_filters_box_name">Əmək haqqı</div>
+                            {/* minimum or maximum salary */}
+                            <div className="vacancies_page_filters_salaries">
+                                {/* minimum salary */}
+                                <input type="number" className="vacancies_page_filters_salary_input" />
+                                {/* maximum salary */}
+                                <input type="number" className="vacancies_page_filters_salary_input" />
+                            </div>
+                        </li>  
+                        {/* experience */}
+                        <li className="vacancies_page_filters_box">
+                            <CustomSelectOption select_option_name={'İş təcrübəsi'} select_option_array={jobExperience} select_update={setJobExperience}/>
+                        </li> 
+                        {/* education level */}
+                        <li className="vacancies_page_filters_box">
+                            <CustomSelectOption select_option_name={'Təhsil səviyyəsi'} select_option_array={educationLevel} select_update={setEducationLevel}/>
+                        </li> 
+                        {/* company  */}
+                        <li className="vacancies_page_filters_box">
+                            <CustomSelectOption select_option_name={'Şirkət'} select_option_array={company} select_update={setCompany}/>
+                        </li> 
+                        {/* job type  */}
+                        <li className="vacancies_page_filters_box">
+                            <CustomSelectOption select_option_name={'İş qrafiki'} select_option_array={jobType} select_update={setJobType}/>
+                        </li> 
+                        {/* special knowledges */}
+                        <li className="vacancies_page_filters_box">
+                            <input type="text" className="vacancies_page_filters_box_text_input" placeholder='Xüsusi biliklər'/>
+                        </li>
+                        {/* vacancy boxes order  */}
+                        <li className="vacancies_page_filters_box">
+                            <CustomSelectOption select_option_name={'Sıralama'} select_option_array={vacancyOrder} select_update={setVacancyOrder}/>
+                        </li> 
                     </ul>
                 </div>
                 {/* vacancies boxes */}
