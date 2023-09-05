8402d2797d75259fcdb84a9367992b823d5c787e
hasanrza888 <hasanrza.888@gmail.com>
2023-09-05 19:37:49 +0400

    ai bot is ready

--- src/components/CV_checker_modal/CV_checker_modal.jsx
+++ src/components/CV_checker_modal/CV_checker_modal.jsx
@@ -5,40 +5,46 @@ import { Link } from 'react-router-dom';
 import { useEffect, useRef, useState } from 'react';
 import { TypeAnimation } from 'react-type-animation';
 function CVCheckerModal({open_cv_checker}) {
-    const [loadingMessages, setLoadingMessages] = useState(false);
+    const [loadingMessages, setLoadingMessages] = useState(true);
     const containerRef = useRef(null);
-    const [message, setMessage] = useState(<TypeAnimation
-        sequence={[
-            "The CV matches the job description and requirements well. However, there are a few areas where the CV may need improvement to increase its chances of being accepted by the recruiter.First, the CV could provide more details on the candidate's work experience, specifically in the area of web development. The CV only provides a brief overview of the candidate's work experience, and does not provide any details on the projects the candidate has worked on or the skills they have developed.Second, the CV does not mention any specific results the candidate has achieved in their previous roles. It would be helpful for the recruiter to know what the candidate has accomplished in their previous roles, in order to get a better sense of their skills and abilities.Third, the CV could provide more detail on the candidate's education, specifically in the area of mathematics. The CV only provides a brief overview of the candidate's education, and does not provide any details on the specific courses the candidate has taken or the skills they have developed.Fourth, the CV could provide more detail on the candidate's skills. The CV only provides a brief overview of the candidate's skills, and does not provide any details on the specific skills the candidate has developed or the level of experience they have with each skill.Suggestions and improvements: The CV should provide more details on the candidate's work experience, specifically in the area of web development. The CV should provide more.",
-            400,
-        ]}
-        speed={50}
-        repeat={0}
-        cursor={false}
-    />)
+    const [message, setMessage] = useState('');
+    const [text, setText] = useState("The CV matches the job description and requirements well. However, there are a few areas where the CV may need improvement to increase its chances of being accepted by the recruiter.First, the CV could provide more details on the candidate's work experience, specifically in the area of web development. The CV only provides a brief overview of the candidate's work experience, and does not provide any details on the projects the candidate has worked on or the skills they have developed.Second, the CV does not mention any specific results the candidate has achieved in their previous roles. It would be helpful for the recruiter to know what the candidate has accomplished in their previous roles, in order to get a better sense of their skills and abilities.Third, the CV could provide more detail on the candidate's education, specifically in the area of mathematics. The CV only provides a brief overview of the candidate's education, and does not provide any details on the specific courses the candidate has taken or the skills they have developed.Fourth, the CV could provide more detail on the candidate's skills. The CV only provides a brief overview of the candidate's skills, and does not provide any details on the specific skills the candidate has developed or the level of experience they have with each skill.Suggestions and improvements: The CV should provide more details on the candidate's work experience, specifically in the area of web development. The CV should provide more.")
     // closes modal
     const close_modal = (e)=>{
         if(e.target.className === 'CV_checker_modal_container'){
             open_cv_checker();
         }
     }
+    const typingSpeed = 50; // Adjust the typing speed as needed
+    useEffect(() => {
+        const typeText = () => {
+            if (text.length > message.length && loadingMessages === false) {
+                setMessage(text.substring(0, message.length + 1));
+            } else {
+                clearInterval(typingInterval);
+            }
+        };
+        const typingInterval = setInterval(typeText, typingSpeed);
+
+        return () => {
+            clearInterval(typingInterval);
+        };
+    }, [text, message, loadingMessages]);
+    useEffect(() => {
+        // Scroll to the end of the container when text updates
+        if(loadingMessages === false){
+            containerRef.current.scrollTop = containerRef.current.scrollHeight;    
+        }
+    }, [message]);
     useEffect(()=>{
-        // setMessage(TextTypingAnimation());
-        containerRef.current.scrollTop = containerRef.current.scrollHeight;
-    },[message])
-    const TextTypingAnimation = (text)=>{
-        return ( 
-            <TypeAnimation
-                sequence={[
-                    "The CV matches the job description and requirements well. However, there are a few areas where the CV may need improvement to increase its chances of being accepted by the recruiter.First, the CV could provide more details on the candidate's work experience, specifically in the area of web development. The CV only provides a brief overview of the candidate's work experience, and does not provide any details on the projects the candidate has worked on or the skills they have developed.Second, the CV does not mention any specific results the candidate has achieved in their previous roles. It would be helpful for the recruiter to know what the candidate has accomplished in their previous roles, in order to get a better sense of their skills and abilities.Third, the CV could provide more detail on the candidate's education, specifically in the area of mathematics. The CV only provides a brief overview of the candidate's education, and does not provide any details on the specific courses the candidate has taken or the skills they have developed.Fourth, the CV could provide more detail on the candidate's skills. The CV only provides a brief overview of the candidate's skills, and does not provide any details on the specific skills the candidate has developed or the level of experience they have with each skill.Suggestions and improvements: The CV should provide more details on the candidate's work experience, specifically in the area of web development. The CV should provide more.",
-                    400,
-                ]}
-                speed={50}
-                repeat={0}
-                cursor={false}
-            />
-         );
-    }
+        const loadData = ()=>{
+            setLoadingMessages(false);
+        }
+        setTimeout(loadData, 3000)
+        return ()=>{
+            clearTimeout(loadData);
+        }
+    }, [])
     return ( 
         <div className="CV_checker_modal_container" onClick={close_modal}>
             <div className="CV_checker_modal">
