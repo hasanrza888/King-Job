88866c189c68ed0a9153e586a1979c31f96cc646
Shikhkarim <sixkerimmemmedov2001@gmail.com>
2023-10-13 00:50:15 +0400

    sdfds

--- src/axiosInstances.js
+++ src/axiosInstances.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const instance = axios.create({
-    baseURL:'https://seal-app-5gg2a.ondigitalocean.app/api',
+    baseURL:'http://localhost:5000/api',
     withCredentials:true,
 });
 
