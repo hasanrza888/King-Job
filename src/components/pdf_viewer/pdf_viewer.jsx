80883a2eb83237f9bd13e940f7e3e7835163a9d5
Shikhkarim <sixkerimmemmedov2001@gmail.com>
2023-10-27 01:43:51 +0400

    fixed pdf-viewer

--- src/components/pdf_viewer/pdf_viewer.jsx
+++ src/components/pdf_viewer/pdf_viewer.jsx
@@ -2,13 +2,14 @@ import { Viewer, Worker } from "@react-pdf-viewer/core";
 import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
-
+import packageJson from '../../../package.json';
 function PdfViewer({fileUrl}) {
-    const newPlugin = defaultLayoutPlugin();
+    const pdfjsVersion = packageJson.dependencies['pdfjs-dist'];
+    const newPlugin = defaultLayoutPlugin()
 
     return ( 
         <div className="pdf_viewer_contianer">
-            <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+            <Worker workerUrl={`https://unpkg.com/pdfjs-dist@${pdfjsVersion}/build/pdf.worker.min.js`}>
                 <Viewer fileUrl={fileUrl} plugins={[newPlugin]} />
             </Worker>
         </div>
