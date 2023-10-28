46bf4d909f4b39b48bea04aae06f70780667f3a1
hasanrza888 <hasanrza.888@gmail.com>
2023-10-28 12:44:31 +0400

    applies page groved

--- src/components/company_profile_components/c_p_applies/c_p_total_applies/c_p_total_applies.jsx
+++ src/components/company_profile_components/c_p_applies/c_p_total_applies/c_p_total_applies.jsx
@@ -2,16 +2,41 @@ import { Link } from 'react-router-dom';
 import './c_p_total_applies.css';
 import PdfViewer from '../../../pdf_viewer/pdf_viewer';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClose } from '@fortawesome/free-solid-svg-icons';
+import { faClose, faFilter, faSearch, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { useEffect, useRef, useState } from 'react';
 import { useSelector,useDispatch } from 'react-redux';
 import {companyAcceptUserApply} from '../../../../apiservices';
 import {toast} from 'react-toastify'
 import { updateUserApply } from '../../../../redux/reducers/companyProfileReducers';
+import CustomSelectOption from '../../../custom_select_option/custom_select_option';
 
 function CpTotalApplies() {
     const [showCV, setShowCV] = useState(false);
     const [CVFile, setCvFile] = useState('');
+    const [searchQuery, setSearchQuery] = useState('');
+    const [openFilters, setOpenFilters] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [filter, setFilter] = useState({
+        vacancyName: ''
+    })
+    const [vacancyName, setVacancyName] = useState([
+        {
+            id: 1,
+            optionName: 'Front End Developer (Unpaid Internship)',
+            value: '',
+            selected: false,
+        },
+        {   
+            id: 2,
+            optionName: 'UI/UX Designer(Unpaid Internship)',
+            selected: false,
+        },
+        {   
+            id: 3,
+            optionName: 'Software QA Engineer (Unpaid internship)',
+            selected: false,
+        }
+    ])
     const {companyJobsApplys:applyes} = useSelector(state=>state.companyProfile);
     const dispatch = useDispatch();
 
@@ -71,48 +96,96 @@ function CpTotalApplies() {
         }
         
     }, [showCV])
-
+    // filters opener function
+    const openFiltersBox = ()=>{
+        setOpenFilters(!openFilters);
+    }
+    // search submit
+    const searchApplySubmit = (e)=>{
+        e.preventDefault();
+        // console.log(e.target.search_form_input.value)
+        setSearchQuery(e.target.search_form_input.value);
+    }
+    console.log(filter)
     return ( 
         <div className="c_p_total_applies_cont">
-            {/* table container */}
-            <div className="c_p_applies">
-                <table className="c_p_applies_table">
-                    <thead>
-                        <tr>
-                            <th>N</th>
-                            <th>Ad</th>
-                            <th>Vakansiya adı</th>
-                            <th>Email</th>
-                            <th>CV faylı</th>
-                            <th>Status</th>
-                            <th>İdarəetmə</th>
-                        </tr>
-                    </thead>
-                    <tbody>                        
-                        {applyes.map((apply,ind) => (
-                            <tr key={apply._id}>
-                                <td>{ind+1}</td>
-                                <td>{apply.userName}</td>
-                                <td>{apply.jobName}</td>
-                                <td><Link to={`mailto:${apply.userEmail}`}>{apply.userEmail}</Link></td>
-                                <td><button className='c_p_open_cv' onClick={()=> { return OpenCvFUnc(apply.file)}}>{"Cv-ə bax"}</button></td>
-                                <td>
-                                    <span className={`c_p_apply_status ${apply.status === "pending" ? 'c_p_apply_status_pending' : apply.status === "approved" ? "c_p_apply_status_accepted" : apply.status === "rejected" ? "c_p_apply_status_rejected" : "c_p_apply_status_thinking"}`}>{apply.status}</span>
-                                </td>
-                                <td className='applies_manage'>
-                                    {apply.status!=='rejected' && <button onClick={()=>acceptuserapply(apply._id,'rejected')}className='c_p_action_button cancel-button'>reject</button>}
-                                    {apply.status!=='rejected' && <button onClick={()=>acceptuserapply(apply._id,'approved')} className='c_p_action_button select-button'>approv</button>}
-                                    {apply.status!=='rejected' && <button onClick={()=>acceptuserapply(apply._id,'thinking')} className='c_p_action_button think-button'>think</button>}
-                                    {/* /* <button className="c_p_action_button cancel-button">Ləğv et</button> */}
-                                    {/* <button onClick={()=>acceptuserapply(apply._id,apply.status)} className="c_p_action_button select-button">Seç</button>
-                                    <button className="c_p_action_button interview-button">Müsahibə dəvəti</button> */}
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+            {/* search applies form input */}
+            <div className="c_p_total_applies_Search_form_and_new_cont">
+                <form onSubmit={searchApplySubmit} className='c_p_total_applies_Search_form'>
+                    <input className='c_p_total_applies_Search_form_input' name='search_form_input' type="text" placeholder='Namizədi axtarın'/>
+                    <button className='c_p_total_applies_Search_form_sbmt' type="submit">
+                        <FontAwesomeIcon icon={faSearch} />
+                    </button>
+                </form>
+                {/* filter button and create vacancy button */}
+                <div className="c_p_total_applies_Search_form_flt_btn_and_link">
+                    <button className="c_p_total_applies_Search_form_open_filters" onClick={openFiltersBox} title='Filter'>
+                        <FontAwesomeIcon icon={faFilter} />
+                    </button>
+                </div>
             </div>
-           
+            {/* filters container */}
+            <div className={`c_p_total_applies_filters_container ${openFilters && 'c_p_total_applies_filters_close'}`}>
+                <ul className='c_p_total_applies_filters'>
+                    <li className='c_p_total_applies_filters_list_item'>
+                        <CustomSelectOption key="vacancy_name" select_option_name={'Vakansiya adı'} select_option_id="vacancyName" subOptionId='' select_option_array={vacancyName} select_update={setVacancyName} filter={filter} setFilter={setFilter}/>
+                    </li>
+                </ul>
+            </div>
+            {/* table container */}
+            {
+                loading ?
+                <div className="c_p_total_applies_loading_rotates">
+                    <FontAwesomeIcon className='c_p_total_applies_loading_rot_icon' icon={faSpinner} />
+                </div>
+                :
+                <>
+                {
+                    applyes.filter((item) => {return item.userName.toLowerCase().includes(searchQuery.toLowerCase()) && item.jobName.toLowerCase().includes(filter.vacancyName.toLowerCase())}).length > 0 ?
+                    
+                    <div className="c_p_applies">
+                        <table className="c_p_applies_table">
+                            <thead>
+                                <tr>
+                                    <th>N</th>
+                                    <th>Ad</th>
+                                    <th>Vakansiya adı</th>
+                                    <th>Email</th>
+                                    <th>CV faylı</th>
+                                    <th>Status</th>
+                                    <th>İdarəetmə</th>
+                                </tr>
+                            </thead>
+                            <tbody>                        
+                                {applyes.filter((item) => {return item.userName.toLowerCase().includes(searchQuery.toLowerCase()) && item.jobName.toLowerCase().includes(filter.vacancyName.toLowerCase())}).map((apply,ind) => (
+                                    <tr key={apply._id}>
+                                        <td>{ind+1}</td>
+                                        <td>{apply.userName}</td>
+                                        <td>{apply.jobName}</td>
+                                        <td><Link to={`mailto:${apply.userEmail}`}>{apply.userEmail}</Link></td>
+                                        <td><button className='c_p_open_cv' onClick={()=> { return OpenCvFUnc(apply.file)}}>{"Cv-ə bax"}</button></td>
+                                        <td>
+                                            <span className={`c_p_apply_status ${apply.status === "pending" ? 'c_p_apply_status_pending' : apply.status === "approved" ? "c_p_apply_status_accepted" : apply.status === "rejected" ? "c_p_apply_status_rejected" : "c_p_apply_status_thinking"}`}>{apply.status}</span>
+                                        </td>
+                                        <td className='applies_manage'>
+                                            {apply.status!=='rejected' && <button onClick={()=>acceptuserapply(apply._id,'rejected')}className='c_p_action_button cancel-button'>reject</button>}
+                                            {apply.status!=='rejected' && <button onClick={()=>acceptuserapply(apply._id,'approved')} className='c_p_action_button select-button'>approv</button>}
+                                            {apply.status!=='rejected' && <button onClick={()=>acceptuserapply(apply._id,'thinking')} className='c_p_action_button think-button'>think</button>}
+                                            {/* /* <button className="c_p_action_button cancel-button">Ləğv et</button> */}
+                                            {/* <button onClick={()=>acceptuserapply(apply._id,apply.status)} className="c_p_action_button select-button">Seç</button>
+                                            <button className="c_p_action_button interview-button">Müsahibə dəvəti</button> */}
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>    
+                    :
+                    <div className="company_profile_my_vacancies_error">MÖVCUD NAMİZƏD YOXDUR!</div>
+                }
+                </>
+            }
+            
             {/* cv modal */}
             {
                 showCV &&
